9315511b00e520f2fd77ef7610f29dc506eb8038
cyrusgis <98184452+cyrusgis@users.noreply.github.com>
2023-12-04 20:02:08 -0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1SuJHMD4eqx8FxsycdxWLiLXsel9aHdnGfaoRO3J9OG8/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1N15X8qbKeNVFIsjMVogh9ASxfOp6o3Mdet_BVW8LJm0/edit#gid=164271551';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyD9EyGsk_lahxv7dEQfT_lO9IwGNyWXHhg';
